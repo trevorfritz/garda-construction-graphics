e92c8cef19e5bcbc93bc88665d81b9fcf15c2454
Trevor Fritz <trevor@fritz4.com>
2021-04-18 16:43:06 -0700

    Completed tool for uploading blobs

---
+++ tools/upload-src/Console/UploadArguments.cs
@@ -0,0 +1,10 @@
+﻿namespace BlobConsoleUpload
+{
+    public class UploadArguments
+    {
+        public string Name { get; set; } = string.Empty;
+        public string FromPath { get; set; } = string.Empty;
+        public string ToConnectionString { get; set; } = string.Empty;
+        public string ToContainer { get; set; } = string.Empty;
+    }
+}
---
+++ tools/upload-src/Console/UploadCounts.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace BlobConsoleUpload
+{
+    public class UploadCounts
+    {
+        public int FileCountToUpload { get; set; }
+        public int FileCountUploaded { get; set; }
+        public Stopwatch Duration { get; set; }
+        public TimeSpan LastUserUpdate { get; set; }
+    }
+}
